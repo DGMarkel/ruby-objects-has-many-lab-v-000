1b284d021e151907559ba2547dbb699134cb083c
David Markel <davidgmarkel@gmail.com>
2018-03-25 16:03:13 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+attr_accessor :name
+@@song_count = []
+
+  def new
+    @name = name
+    @songs = []
+  end 
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def add_song_by_name(name)
+  end
+
+  def song_count
+  end
+
+end
