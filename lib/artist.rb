bcbef7b83bb7545782308b6f16ad422e94f72399
David Markel <davidgmarkel@gmail.com>
2018-03-25 16:38:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-attr_accessor :name
+attr_accessor :name 
 @@song_count = []
 
   def initialize(name)
@@ -13,6 +13,7 @@ attr_accessor :name
 
   def add_song(song)
     @songs << song
+    song.artist = self
   end
 
   def add_song_by_name(name)
