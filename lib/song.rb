c328342488e0afddf24f49649181b0a369e7d08d
David Markel <davidgmarkel@gmail.com>
2018-03-25 16:18:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
 attr_accessor :name, :artist
 
-  def initialize(name, artist = nil)
+  def initialize(name)
     @name = name
     @artist = artist
   end
@@ -10,8 +10,9 @@ attr_accessor :name, :artist
     @name
   end
 
-  def artist
-    @artist
+  def artist(artist)
+    @artist = Artist.new
+    
   end
 
   def artist_name
